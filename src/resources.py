24c5288a232badcf8a7fa759653812a95932cb69
jspmic <micaeljaspe@gmail.com>
2025-06-23 22:07:06 +0200

    Feat: added UtilisateurSchema

--- src/resources.py
+++ src/resources.py
@@ -1,4 +1,27 @@
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import Resource, marshal_with
+from marshmallow import Schema, fields
+
+
+# Schemas Definition
+
+class UtilisateurSchema(Schema):
+    nom = fields.Str()
+    dateNaissance = fields.Date()
+    email = fields.Email()
+    numeroTelephone = fields.Str()
+    province = fields.Str()
+    commune = fields.Str()
+
+
+# Resources definition
+
+class UtilisateurResource(Resource):
+    def get(self):
+        pass
+
+    @marshal_with(UtilisateurSchema)
+    def post(self, admin_code: int):
+        pass
 
 
 class Home(Resource):
