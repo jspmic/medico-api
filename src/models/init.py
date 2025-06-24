2ecfd2fe96da35cff0c1f0aaaca5cd40ddcf5f12
jspmic <micaeljaspe@gmail.com>
2025-06-24 11:49:33 +0200

    fix: handled the error raised when failure occurs when connecting to the database
    - added comments for inline documentation

--- src/models/init.py
+++ src/models/init.py
@@ -2,15 +2,24 @@ import os
 from flask_restful import Api
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask
+import logging
 
 find_dotenv("../../.env")
 load_dotenv()
+
+# Constants definition
 
 HOST = os.getenv("HOST")
 USER = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
+# Logging setup
+
+logger = logging.getLogger(__name__)
+logging.basicConfig("log.log", encoding="utf-8", level=logging.DEBUG)
+
+# App setup
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,5 +27,7 @@ api = Api(app)
 try:
     app.config["SQLALCHEMY_DATABASE_URI"] = \
             f"mysql://{USER}:{PASSWORD}@{HOST}/{DB_NAME}"
-except Exception:
-    pass
+    logger.info("Connected to database")
+except Exception as e:
+    logger.error(f"Encountered error when connecting to database: {e}")
+    raise e
