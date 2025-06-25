9b834d18d0da5d91e59012e8f2d5128e4c029a56
jspmic <micaeljaspe@gmail.com>
2025-06-25 08:17:05 +0200

    feat: added UtilisateurResource on route /user

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from src.models.init import HOST, \
         api, app
-from src.resources import Home
+from src.resources import Home, UtilisateurResource
 from src.models.models import db
 
 
 api.add_resource(Home, "/")
+api.add_resource(UtilisateurResource, "/user")
