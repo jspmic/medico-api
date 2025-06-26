bf4b8afefcb035a188d9267e034a90be76cca015
jspmic <micaeljaspe@gmail.com>
2025-06-26 13:45:34 +0200

    fix: added password and sexe in the column of the model Utilisateur

--- src/models/models.py
+++ src/models/models.py
@@ -22,20 +22,26 @@ class Utilisateur(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nom = db.Column(db.String(254), nullable=False)
     dateNaissance = db.Column(db.Date, nullable=False)
+    sexe = db.Column(db.String(1), nullable=False)
     email = db.Column(db.String(254), nullable=True)
     numeroTelephone = db.Column(db.String(254), nullable=True)
     province = db.Column(db.String(254), nullable=False)
     commune = db.Column(db.String(254), nullable=False)
     password = db.Column(db.String(65), nullable=False)
 
-    def to_dict(self):
+    def get_identity(self):
+        return self.nom
+
+    def to_dict(self, access_token: str = ""):
         return {
                 "id": self.id,
                 "nom": self.nom,
+                "sexe": self.sexe,
                 "dateNaissance": self.dateNaissance,
                 "email": self.email,
                 "numeroTelephone": self.numeroTelephone,
                 "province": self.province,
                 "commune": self.commune,
-                "password": self.password
+                "password": self.password,
+                "access_token": access_token
                 }
