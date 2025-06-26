eb538169e919ecdc77c08ec6731ff939f3413f16
jspmic <micaeljaspe@gmail.com>
2025-06-26 13:44:34 +0200

    feat: added Resource Test to test a protected route

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from src.models.init import HOST, \
         api, app
-from src.resources import Home, UtilisateurResource
+from src.resources import Home, UtilisateurResource, \
+        Test
 from src.models.models import db
 
 
 api.add_resource(Home, "/")
 api.add_resource(UtilisateurResource, "/user")
+api.add_resource(Test, "/test")
