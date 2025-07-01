3a71dac527d34fca9218003863e36f6c67311f29
jspmic <micaeljaspe@gmail.com>
2025-07-01 11:37:55 +0200

    fix: removed endpoint /services and added /hopital

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
 from src.models.init import HOST, \
         api, app
 from src.resources import Home, UtilisateurResource, \
-        Test, Services
+        Test, Hopitals
 from src.models.models import db
 
 
 api.add_resource(Home, "/")
 api.add_resource(UtilisateurResource, "/user")
 api.add_resource(Test, "/test")
-api.add_resource(Services, "/services")
+api.add_resource(Hopitals, "/hopital")
