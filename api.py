0a684418eec7024defbea5bedfd50655fdcc6020
jspmic <micaeljaspe@gmail.com>
2025-07-02 22:21:03 +0200

    feat: added RDV to an endpoint

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from src.models.init import HOST, \
         api, app
 from src.resources import Home, UtilisateurResource, \
-        Test, Hopitals
+        Test, Hopitals, RDVs
 from src.models.models import db
 
 
@@ -9,3 +9,4 @@ api.add_resource(Home, "/")
 api.add_resource(UtilisateurResource, "/user")
 api.add_resource(Test, "/test")
 api.add_resource(Hopitals, "/hopital")
+api.add_resource(RDVs, "/rdv")
